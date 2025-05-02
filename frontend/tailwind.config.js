9b438757ba104e38e827d62494edc1f34f7af717
mateberidze101 <beridzemate545@gmail.com>
2025-05-02 18:52:41 +0400

    start project

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: [
+      './src/**/*.{js,ts,jsx,tsx}',  // Adjust this path to your files
+      './index.html',
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
